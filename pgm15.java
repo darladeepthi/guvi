f4ec292e69a628031c151c09be782be1660bfc07
darladeepthi <42029119+darladeepthi@users.noreply.github.com>
2018-08-13 10:41:32 +0530

    Create pgm15.java

---
+++ pgm15.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i,s,e;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextInt();
+		e=sc.nextInt();
+		for(i=s+1;i<e;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i+"\t");
+				
+			}
+		}
+	}
+}
