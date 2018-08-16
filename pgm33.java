a503694a5cad05224ff6a0fc191f4ef5f2701439
darladeepthi <42029119+darladeepthi@users.noreply.github.com>
2018-08-16 20:51:09 +0530

    Create pgm33.java

---
+++ pgm33.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class pgm33
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String s = "welcome to candid java tutorial";
+ 
+		int count = 0;
+ 
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+			{
+				count++;
+ 
+			}
+		}
+ 		System.out.println(count);
+	}
+}
+	
