c4c59ffbf36abd2db06e30f13fb250eafb762400
darladeepthi <42029119+darladeepthi@users.noreply.github.com>
2018-08-29 15:22:51 +0530

    Create pgm52.java

---
+++ pgm52.java
@@ -0,0 +1,67 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i,n;
+		Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	for(i=1;i<=1;i++)
+	{
+		if(n==1)
+		{
+			System.out.println("One");
+		}
+		else if(n==2)
+		{
+			System.out.println("Two");
+		}
+		else if(n==3)
+		{
+			System.out.println("three");
+		}
+		else if(n==4)
+		{
+			System.out.println("four");
+		}
+		else if(n==5)
+		{
+			System.out.println("five");
+		}
+		else if(n==6)
+		{
+			System.out.println("six");
+		}
+		else if(n==7)
+		{
+			System.out.println("seven");
+		}
+		else if(n==8)
+		{
+			System.out.println("eight");
+		}
+		else if(n==9)
+		{
+			System.out.println("nine");
+		}
+	
+		else
+		{
+		System.out.println("ten");
+		}
+		}
+		}
+}
+		
+		
+		
+		
+	
+	
