b6c0898d93c595ae62236b8273abed75a2d9669d
darladeepthi <42029119+darladeepthi@users.noreply.github.com>
2018-08-29 15:41:19 +0530

    Create pgm53.java

---
+++ pgm53.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,r,sum=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		while(n!=0)
+		{
+			r=n%10;
+			sum=sum+r;
+			n=n/10;
+		}
+		System.out.println(sum);	
+		
+	}
+}
