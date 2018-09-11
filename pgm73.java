f8dd7d5f7aaf0eb0e1dd92c65211558b3364eacf
darladeepthi <42029119+darladeepthi@users.noreply.github.com>
2018-09-11 12:37:44 +0530

    Create pgm73.java

---
+++ pgm73.java
@@ -0,0 +1,37 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int k,s,e,i;
+		boolean flag=false;
+		Scanner sc=new Scanner(System.in);
+		k=sc.nextInt();
+		s=sc.nextInt();
+		e=sc.nextInt();
+		for(i=s+1;i<e;i++);
+		{
+			if(k==i)
+			{
+				flag=true;
+			}
+
+		}
+			if(flag=true)
+			{
+				System.out.println("yes");
+			}
+			else
+			{
+System.out.println("no");				
+			}
+			}
+			}
+			
