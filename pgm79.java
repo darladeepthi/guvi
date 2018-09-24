cfcd7856234157d7f6c1f5d4f28e59068ab26c12
darladeepthi <42029119+darladeepthi@users.noreply.github.com>
2018-09-24 16:19:00 +0530

    Create pgm79.java

---
+++ pgm79.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		       int b;
+        Scanner s=new Scanner(System.in);
+       b=s.nextInt();
+      int c=s.nextInt();
+      int h=b*c;
+      double j=Math.sqrt(h);
+      if(j==c){
+          System.out.println("Yes");
+      }
+ else{
+System.out.println("No");
+	}
+}
+}
