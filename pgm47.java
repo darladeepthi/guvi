9d5e5da158a5bdb0f5dc4848e95270ec16d1062a
darladeepthi <42029119+darladeepthi@users.noreply.github.com>
2018-09-25 15:27:58 +0530

    Create pgm47.java

---
+++ pgm47.java
@@ -0,0 +1,40 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,i,max,min;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		max=a[n-1];
+		min=a[0];
+		for(i=0;i<n;i++)
+		{
+			if(min>a[i])
+			{
+				min=a[i];
+			}
+		}
+			for(i=0;i<n;i++)
+			{
+				if(max<a[i])
+				{
+					max=a[i];
+				}
+			}
+			System.out.println(min+"\t"+max);
+				
+				}
+			}
